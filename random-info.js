1b1e19f82a7a93356bcd15f58ecdcf5ef1178d5b
muelleoa <muelleoa@mail.uc.edu>
2024-04-01 23:53:00 -0400

    Updated JS info

--- random-info.js
+++ random-info.js
@@ -1,9 +1,15 @@
 // List of possible choices
-
+function showInfo() {
+   var infoList = [
+      "Ivan",
+      "Cookie",
+      "Vlad",
+      "<h2>Hello World!</h2>"
+   ];
 
    // Generate a random number and pick that item from the array
-
+   var randomizer = Math.floor(Math.random() * infoList.length)
    
    // Write that information in the HTML element that has the ID of "info"
-
-   
+   document.getElementById("info").innerHTML = infoList[randomizer];
+};
